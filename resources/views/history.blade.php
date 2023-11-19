9d4024ec6c6c472076a285bbcc0c126d7fa8b201
ukarsh2004git <utkarshshahare2004@gmail.com>
2023-11-19 17:31:48 +0530

    added History Page

--- resources/views/history.blade.php
+++ resources/views/history.blade.php
@@ -1,8 +1,30 @@
 @include('layouts.header')
 @push('title')
 <title>History</title>
-<style>
-
-</style>
-<h1>This is History</h1>
+@vite('resources/css/forHistory.css')
+<div class="content">
+    <div class="heading">
+        <h3><span>History of Dr. B.R. Ambedkar</span></h3>
+    </div>
+    <div class="paras">
+    <div class="para para1">
+    Dr. B.R. Ambedkar, a towering figure in Indian history, dedicated his life to fighting against social injustice and championing the cause of the oppressed. Born into the Mahar caste, traditionally considered untouchable, he faced discrimination from a young age. Despite societal barriers, Ambedkar displayed exceptional academic prowess, earning degrees in economics and political science from Bombay University and pursuing further studies in London and Bonn.
+    </div>
+    <div class="para para2">
+    Ambedkar's commitment to social reform and activism became evident early in his life. He led movements such as the Mahad Satyagraha in 1927, advocating for the right of untouchables to access public water sources. His efforts also extended to the Temple Entry Movement, challenging the exclusion of untouchables from Hindu temples. These endeavors marked the beginning of a lifelong struggle against caste-based discrimination.
+    </div>
+    <div class="para para3">
+    However, Ambedkar's most enduring legacy lies in his pivotal role in the drafting of the Constitution of India. As the Chairman of the Drafting Committee of the Constituent Assembly, he steered the framing of a visionary and egalitarian constitution for the newly independent nation. His commitment to principles of justice, equality, and fundamental rights is reflected in the foundational document that continues to govern the world's largest democracy.
+    </div>
+    <div class="para para4">
+    Ambedkar's influence extended into politics, where he served as the first Law Minister of independent India in Prime Minister Jawaharlal Nehru's cabinet. His political career was characterized by a relentless pursuit of the rights of Scheduled Castes and Scheduled Tribes, advocating for affirmative action policies to uplift marginalized communities.
+    </div>
+    <div class="para para5">
+    In a profound personal and symbolic gesture, Ambedkar, disillusioned with the caste-based inequalities in Hinduism, embraced Buddhism on October 14, 1956. This mass conversion ceremony, known as Dhamma Chakra Pravartan Din, marked a turning point in his spiritual journey and inspired countless followers to follow suit.  
+    </div>
+    <div class="para para6">
+    Dr. B.R. Ambedkar's legacy endures in the socio-political fabric of India. His ideas and contributions continue to shape conversations about social justice, caste equality, and the empowerment of marginalized communities. Ambedkar's birthday, celebrated annually as Ambedkar Jayanti, serves as a reminder of his indomitable spirit and unwavering commitment to building a more inclusive and just society.
+    </div>
+    </div>
+</div>
 @include('layouts.footer')
