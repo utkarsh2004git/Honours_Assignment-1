c43fc5c237283983c7a21e966b2c21a707040b4d
ukarsh2004git <utkarshshahare2004@gmail.com>
2023-11-19 18:27:25 +0530

    added Book Page

--- resources/views/book.blade.php
+++ resources/views/book.blade.php
@@ -1,9 +1,37 @@
 @include('layouts.header')
 @push('title')
 <title>Book</title>
-<style>
-</style>
+@vite('resources/css/forBook.css')
 <div class="content">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos aut asperiores nesciunt illum dolor. Dignissimos quos commodi modi expedita delectus facere laborum, consectetur, suscipit dolore unde temporibus, impedit excepturi a eum nulla? Ipsum autem nihil suscipit ex sint ea officia et qui, quae inventore modi atque sed iusto. Esse autem minus sequi, corrupti harum molestias quam laudantium mollitia fuga expedita eos doloribus deleniti natus eum tempora dolores iusto adipisci quidem nobis error architecto illo eveniet perspiciatis a. Nam suscipit molestiae similique incidunt illo cumque, officiis adipisci sint, ipsa sed tempora corrupti, reprehenderit pariatur praesentium earum dolorum! Ad vitae nobis architecto?
+    <div class="heading">
+        <h3>Books Written By Dr. B.R. Ambedkar</h3>
+    </div>
+
+    <div class="BOOKS">
+        <div class="book1 book">
+            <img src="images/books/one.jpg" alt="img">
+            <h4>Annihilation of Caste</h4>
+        </div>
+        <div class="book2 book">
+            <img src="images/books/two.jpg" alt="img">
+            <h4>The Problem of the Rupee</h4>
+        </div>
+        <div class="book3 book">
+            <img src="images/books/three.jpg" alt="img">
+            <h4>Thoughts on Linguistic States</h4>
+        </div>
+        <div class="book4 book">
+            <img src="images/books/four.jpg" alt="img">
+            <h4>The Buddha and His Dhamma</h4>
+        </div>
+        <div class="book5 book">
+            <img src="images/books/five.jpg" alt="img">
+            <h4>Waiting for a Visa</h4>
+        </div>
+        <div class="book6 book">
+            <img src="images/books/six.jpg" alt="img">
+            <h4>Riddles in Hinduism</h4>
+        </div>
+    </div>
 </div>
 @include('layouts.footer')
